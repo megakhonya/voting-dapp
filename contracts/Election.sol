c3d2aae10eaf319209880168cace5841de051ee1
Meg Akhonya <musinzimeg@gmail.com>
2024-03-14 14:44:22 +0300

    1. Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;
+
+contract Election {
+    // Read/write candidate
+    string public candidate;
+
+    // Constructor
+    constructor () {
+        candidate = "Candidate 1";
+    }
+}
